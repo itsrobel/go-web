2f38f181e25b62bf83b8ee9bf171aaf662506aff
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-11-17 22:27:21 -0800

    added a simple UI using templ and daisyUI

---
+++ Makefile
@@ -0,0 +1,30 @@
+# Makefile
+
+# Variables
+TEMPL_DIR := internal/templates
+GENERATED_DIR := generated
+GO_FILES := $(wildcard *.go)
+TEMPL_FILES := $(wildcard $(TEMPL_DIR)/*.templ)
+GENERATED_FILES := $(patsubst $(TEMPL_DIR)/%.templ,$(GENERATED_DIR)/%_templ.go,$(TEMPL_FILES))
+
+# Default target
+all: generate move
+
+# Generate Templ files
+generate:
+	@echo "Generating Templ files..."
+	@templ generate
+
+# Move generated files to subdirectory
+move:
+	echo "Moving generated files to $(GENERATED_DIR)..." \
+	&& mkdir -p $(TEMPL_DIR)/$(GENERATED_DIR) \
+	&& mv $(TEMPL_DIR)/*_templ.go $(TEMPL_DIR)/$(GENERATED_DIR)/ 2>/dev/null || true
+
+# Clean generated files
+clean:
+	@echo "Cleaning generated files..."
+	@rm -rf $(GENERATED_DIR)
+
+# Phony targets
+.PHONY: all generate move clean
---
+++ watcher/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	// "fmt"
+	"os"
+	"path/filepath"
+
+	// web server support
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/russross/blackfriday/v2"
+)
+
+func main() {
+	watcher()
+	server()
+}
+
+func server() {
+	r := gin.Default()
+
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(s string) template.HTML {
+			return template.HTML(s)
+		},
+	})
+
+	// Load HTML templates
+	r.LoadHTMLGlob("templates/*")
+
+	// Serve static files
+	r.Static("/static", "./static")
+
+	// Handle root route
+	r.GET("/", serveMarkdown("index.md"))
+
+	// Handle other Markdown files
+	r.GET("/:page", serveMarkdown(""))
+
+	// Handle HTMX requests for page content
+	r.GET("/content/:page", serveMarkdownContent)
+	r.POST("/clicked", handleClicked)
+
+	println("Server running on port 8080")
+
+	r.Run(":8080")
+}
+
+var clickCount = 0
+
+// func handleClicked(c *gin.Context) {
+// 	clickCount++
+// 	c.String(http.StatusOK, fmt.Sprintf("Clicked %d times", clickCount))
+// }
+
+func serveMarkdown(defaultFile string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		page := c.Param("page")
+		if page == "" {
+			page = defaultFile
+		} else {
+			page += ".md"
+		}
+
+		content, err := os.ReadFile(filepath.Join("content", page))
+		if err != nil {
+			c.String(http.StatusNotFound, "Page not found")
+			return
+		}
+
+		// Convert Markdown to HTML
+		html := blackfriday.Run(content)
+
+		// Render the HTML within the layout template
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"content": string(html),
+		})
+	}
+}
+
+func serveMarkdownContent(c *gin.Context) {
+	page := c.Param("page")
+	content, err := os.ReadFile(filepath.Join("content", page+".md"))
+	if err != nil {
+		c.String(http.StatusNotFound, "Page not found")
+		return
+	}
+
+	// Convert Markdown to HTML
+	html := blackfriday.Run(content)
+
+	c.Header("Content-Type", "text/html")
+	c.String(http.StatusOK, string(html))
+}
